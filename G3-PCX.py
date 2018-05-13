fbab2ddce51d531b6be8b1a382b84b789fe78b69
Rohitash Chandra <c.rohitash@gmail.com>
2018-05-13 23:08:04 +1000

    converges, but need to improve. checked pcx with c code

--- G3-PCX.py
+++ G3-PCX.py
@@ -58,15 +58,27 @@ class evolution:
 
 		self.max_evals = max_evals
 
+		self.problem = 2
 
 
 
-	def fit_func(self, x):    # rosenbrock function
+ 
+
+	def fit_func(self, x):    #   function  (can be any other function, model or even a neural network)
 		fit = 0
-		for j in range(x.size -1): 
-			fit += 100.0*(x[j]*x[j] - x[j+1])*(x[j]*x[j] - x[j+1]) + (x[j]-1.0)*(x[j]-1.0)
 
-		return  fit
+		if self.problem == 1: # rosenbrock
+			for j in range(x.size -1): 
+				fit  = fit +  (100.0*(x[j]*x[j] - x[j+1])*(x[j]*x[j] - x[j+1]) + (x[j]-1.0)*(x[j]-1.0))
+
+		elif self.problem ==2:  # ellipsoidal - sphere function
+			for j in range(x.size):
+				fit= fit + ((j+1)*(x[j]*x[j]))
+ 
+				  
+
+		return fit # note we will maximize fitness, hence minimize error 
+
 
 	def evaluate(self): 
 
@@ -85,18 +97,7 @@ class evolution:
 
 
  
-  
-	'''def RandomParents(self):
-		swp = self.temp_index[0]
-		self.temp_index[0] = self.temp_index[self.best_index]
-		self.temp_index[self.best_index] = swp
-
-		for i in range(1, self.num_parents):
-			index = random.randint(1, self.pop_size - 1)
-			swp = self.temp_index[index]
-			self.temp_index[index] = self.temp_index[i]
-			self.temp_index[i] = swp'''
-
+   # calculates the magnitude of a vector
 	def mod(self, List):
 		sum = 0
 		for i in range(self.dimen):
@@ -124,13 +125,18 @@ class evolution:
 		print centroid, ' Centroid'
 
 
+  # calculate the distace (d) from centroid to the index parent self.temp_index[0]
+  # also distance (diff) between index and other parents are computed
 
 		for j in range(1, self.num_parents):
-			
-			if j == 1:
-				d= centroid  - self.population[self.temp_index[0],:]
-			diff[j, :] = self.population[self.temp_index[j], :] - self.population[self.temp_index[0],:]
-			 
+			for i in range(self.dimen):
+				if j == 1:
+					d[i]= centroid[i]  - self.population[self.temp_index[0],i]
+				diff[j, i] = self.population[self.temp_index[j], i] - self.population[self.temp_index[0],i]
+
+			if (self.mod(diff[j,:]) < self.EPSILON):
+				print 'Points are very close to each other. Quitting this run' 
+				#return 0 
 
 
 		dist = self.mod(d) 
@@ -141,7 +147,7 @@ class evolution:
 
 		# orthogonal directions are computed
 		for j in range(1, self.num_parents):
-			temp1 = np.inner(diff[j,:] , d )
+			temp1 = self.inner(diff[j,:] , d )
 
 			if ((self.mod(diff[j,:]) * dist) == 0):
 				print "Division by zero"
@@ -154,39 +160,56 @@ class evolution:
 
 			D[j] = self.mod(diff[j]) * np.sqrt(np.abs(temp3))
 
-		D_not = 0
+ 
+ 
+		D_not = 0.0
 		for i in range(1, self.num_parents):
 			D_not += D[i]
 		D_not /= (self.num_parents - 1) # this is the average of the perpendicular distances from all other parents (minus the index parent) to the index vector
 
 		#for j in range(self.dimen):
-		tempar1 = np.random.normal(0, self.sigma_eta, self.dimen) #rand_normal(0, D_not * sigma_eta);
+		tempar1 = np.random.normal(0,  self.sigma_eta, self.dimen) #rand_normal(0, D_not * sigma_eta);
 		tempar2 = tempar1 
-
-		#for j in range(self.dimen):
+ 
 		if(np.power(dist, 2) == 0):
 			print " division by zero: part 2"
-			tempar2  = tempar1  - (    np.multiply(np.inner(tempar1, d) , d )  )
+			tempar2  = tempar1  
 		else:
-			tempar2  = tempar1  - (    np.multiply(np.inner(tempar1, d) , d )  ) / np.power(dist, 2.0) 
+			tempar2  = tempar1  - (    np.multiply(self.inner(tempar1, d) , d )  ) / np.power(dist, 2.0) 
  
-		tempar1= tempar2 
+		tempar1= tempar2  
+ 
+  
  
 		self.sub_pop[current,:] = self.population[self.temp_index[0],:] + tempar1  
 
-		#temp_rand = np.random.normal(0, self.sigma_zeta, self.dimen)
- 
-		#self.sub_pop[current,:] += np.multiply(temp_rand ,  d )
- 
+		temp_rand = np.random.normal(0, self.sigma_zeta, self.dimen) 
 
+		#for i in range(0, self.dimen):
+		#	self.sub_pop[current,:i] +=  (temp_rand[i] * d[i])
  
- 
+		self.sub_pop[current,:] += np.multiply(temp_rand ,  d ) 
 
+
+  # the child is included in the newpop and is evaluated
+		self.sp_fit[current] = self.fit_func(self.sub_pop[current,:])
+		self.num_evals += 1
+ 
 
 
 		return 1
 
-	def family_members(self):
+
+	def inner(self, ind1, ind2):
+
+		sum = 0.0
+		for i in range(self.dimen):
+			sum += (ind1[i] * ind2[i] )
+		return  sum 
+ 
+ 
+
+	def family_members(self): #//here a random family (1 or 2) of parents is created who would be replaced by good individuals
 
 		swp = 0
 
@@ -223,7 +246,7 @@ class evolution:
 		print self.list, ' sort_population list after '
 
 
-	def replace_parents(self):
+	def replace_parents(self): #here the best (1 or 2) individuals replace the family of parents
 
 		for j in range(self.family):
 			self.population[ self.parents[j],:]  =  self.sub_pop[ self.list[j],:] # Update population with new species 
@@ -232,7 +255,8 @@ class evolution:
 			self.fitness[self.parents[j]]   =  fitness #self.sp_fit[self.list[j]]
 			self.num_evals += 1
 
-	def find_parents(self):
+	def find_parents(self): #here the parents to be replaced are added to the temporary subpopulation to assess their goodness against the new solutions formed which will be the basis of whether they should be kept or not 
+
 
 		self.family_members()
 
@@ -242,8 +266,7 @@ class evolution:
 			self.sp_fit[self.children + j]  = fitness
 
 			print j, fitness, ' find_parents fit'
-			self.num_evals += 1
-			#self.sp_fit[self.children + j] = self.fitness[self.parents[j]]
+			self.num_evals += 1 
 
  
 
@@ -356,8 +379,8 @@ class evolution:
 
 			print self.num_evals, self.best_index, self.best_fit, ' best so far'
 
-		print self.sub_pop, '  sub_pop'
-		print self.population[self.best_index], ' best sol'
+			print self.sub_pop, '  sub_pop'
+			print self.population[self.best_index], ' best sol'
 		print self.fitness, ' fitness'
  
  
@@ -373,9 +396,9 @@ def main():
 
 	random.seed(time.time())
 
-	max_evals = 1000 # need to decide yourself 80000
+	max_evals = 10000# need to decide yourself 80000
 
-	pop_size = 50
+	pop_size =  50
 	num_varibles = 10
 
 	max_limits = np.repeat(5, num_varibles) 
