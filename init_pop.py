8eb5f10a55f56675b3fbbbd41a048cb97edbfc66
arpit-kapoor <kapoor.arpit97@gmail.com>
2018-08-20 12:40:34 +1000

    initialize population for both c++ and python code

--- init_pop.py
+++ init_pop.py
@@ -1,7 +1,10 @@
 import numpy as np
 import sys
+import time
+from shutil import copyfile
 pop_size = int(sys.argv[1])
 num_var = int(sys.argv[2])
+np.random.seed(int(time.time()))
 pop = np.random.uniform(-5, 5, size=(pop_size,num_var))
 pop = np.around(pop, decimals=2)
 with open('pop.txt', 'w') as file:
@@ -9,3 +12,4 @@ with open('pop.txt', 'w') as file:
         for j in range(num_var):
             file.write(str(pop[i, j])+' ')
         file.write('\n')
+copyfile('pop.txt', 'g3pcx_cpp/pop.txt')
